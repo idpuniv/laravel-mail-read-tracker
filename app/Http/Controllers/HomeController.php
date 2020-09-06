aa749a5be453e5e6a5711a3ebeb367ee08d22ab0
optimus prime <idpuniv@gmail.com>
2020-09-06 01:40:25 +0100

    gmail password changed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -125,7 +125,7 @@ class HomeController extends Controller
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
         $mail->Username   = 'paulido92@gmail.com';                     // SMTP username
-        $mail->Password   = 'IdP#i@gm.com95';                               // SMTP password
+        $mail->Password   = 'IdP#i@gm.com95@';                               // SMTP password
         $mail->SMTPSecure = 'tls';         // Enable TLS enryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
