69995d0cebc19fa51057dd3217a6c52dd1464df5
Paul IDO <paulido92@gmail.com>
2020-09-11 15:16:54 -1200

    comment STMPDebug

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -122,7 +122,7 @@ class HomeController extends Controller
      try {
         //Server settings
 
-        $mail->SMTPDebug = 2;                      // Enable verbose debug output
+        //$mail->SMTPDebug = 2;                      // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
